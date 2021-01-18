cd3767623b00ebc223cdbecd42dee7026217c2d2
James Clark <jameswclark@gmail.com>
2021-01-18 13:49:27 -0500

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
 var path = require('path');
 var config = require('./package.json');
 
-var production = process.argv.indexOf('-p') !== -1;
-
-var filename = 'warp-field-' + config.version
-if (production) {
-    filename += '.min.js';
-} else {
-    filename += '.js';
-}
-
-module.exports = {
-    entry: "./src/index.ts",
-    mode: production ? 'production' : 'development',
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'bin'),
-        publicPath: '/bin/',
-        filename: filename,
-        libraryTarget: "var",
-        library: "WarpField"
-    },
-    resolve: {
-        extensions: ['.ts']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [ 'ts-loader' ]
-            }
-        ]
+module.exports = (env, argv) => {
+    var filename = 'warp-field-' + config.version
+    if (argv.mode === 'production') {
+        filename += '.min.js';
+    } else {
+        filename += '.js';
     }
+    return {
+        entry: "./src/index.ts",
+        devtool: 'source-map',
+        output: {
+            path: path.resolve(__dirname, 'bin'),
+            publicPath: '/bin/',
+            filename: filename,
+            libraryTarget: "var",
+            library: "WarpField"
+        },
+        resolve: {
+            extensions: ['.ts']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [ 'ts-loader' ]
+                }
+            ]
+        }
+    };
 };
