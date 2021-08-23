5763bb15b23101f3c7e4355fe86f91e9081db278
James Clark <jameswclark@gmail.com>
2021-08-22 21:14:24 -0400

    bump version to 2.0.2

--- dist/warp-field-2.0.2.js
+++ dist/warp-field-2.0.2.js
@@ -1,4 +1,4 @@
-var WarpField;WarpField =
+var WarpField;
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
@@ -2243,33 +2243,6 @@ exports.FieldOfViewMap = FieldOfViewMap;
 
 /***/ }),
 
-/***/ "./src/index.ts":
-/*!**********************!*\
-  !*** ./src/index.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-/*
- *  WarpField
- *  github.com/sbj42/warp-field
- *  James Clark
- *  Licensed under the MIT license.
- */
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.Offset = exports.CardinalDirectionFlags = exports.CardinalDirection = exports.computeFieldOfView = exports.FieldOfViewMap = void 0;
-var field_of_view_map_1 = __webpack_require__(/*! ./field-of-view-map */ "./src/field-of-view-map.ts");
-Object.defineProperty(exports, "FieldOfViewMap", ({ enumerable: true, get: function () { return field_of_view_map_1.FieldOfViewMap; } }));
-var compute_1 = __webpack_require__(/*! ./compute */ "./src/compute.ts");
-Object.defineProperty(exports, "computeFieldOfView", ({ enumerable: true, get: function () { return compute_1.computeFieldOfView; } }));
-var tiled_geometry_1 = __webpack_require__(/*! tiled-geometry */ "./node_modules/tiled-geometry/lib/index.js");
-Object.defineProperty(exports, "CardinalDirection", ({ enumerable: true, get: function () { return tiled_geometry_1.CardinalDirection; } }));
-Object.defineProperty(exports, "CardinalDirectionFlags", ({ enumerable: true, get: function () { return tiled_geometry_1.CardinalDirectionFlags; } }));
-Object.defineProperty(exports, "Offset", ({ enumerable: true, get: function () { return tiled_geometry_1.Offset; } }));
-
-
-/***/ }),
-
 /***/ "./src/tile-flags.ts":
 /*!***************************!*\
   !*** ./src/tile-flags.ts ***!
@@ -2547,8 +2520,9 @@ function wedgesMatch(a, b) {
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
@@ -2565,10 +2539,34 @@ function wedgesMatch(a, b) {
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	// module exports must be returned from runtime so entry inlining is disabled
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__("./src/index.ts");
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+/*
+ *  WarpField
+ *  github.com/sbj42/warp-field
+ *  James Clark
+ *  Licensed under the MIT license.
+ */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.Offset = exports.CardinalDirectionFlags = exports.CardinalDirection = exports.computeFieldOfView = exports.FieldOfViewMap = void 0;
+var field_of_view_map_1 = __webpack_require__(/*! ./field-of-view-map */ "./src/field-of-view-map.ts");
+Object.defineProperty(exports, "FieldOfViewMap", ({ enumerable: true, get: function () { return field_of_view_map_1.FieldOfViewMap; } }));
+var compute_1 = __webpack_require__(/*! ./compute */ "./src/compute.ts");
+Object.defineProperty(exports, "computeFieldOfView", ({ enumerable: true, get: function () { return compute_1.computeFieldOfView; } }));
+var tiled_geometry_1 = __webpack_require__(/*! tiled-geometry */ "./node_modules/tiled-geometry/lib/index.js");
+Object.defineProperty(exports, "CardinalDirection", ({ enumerable: true, get: function () { return tiled_geometry_1.CardinalDirection; } }));
+Object.defineProperty(exports, "CardinalDirectionFlags", ({ enumerable: true, get: function () { return tiled_geometry_1.CardinalDirectionFlags; } }));
+Object.defineProperty(exports, "Offset", ({ enumerable: true, get: function () { return tiled_geometry_1.Offset; } }));
+
+})();
+
+WarpField = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=warp-field-2.0.0.js.map
+//# sourceMappingURL=warp-field-2.0.2.js.map
